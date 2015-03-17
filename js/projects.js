c58109b7ed744449006e007ced97748dd71f137e
Collin Stedman <cstedman@princeton>
2015-03-16 19:52:14 -0500

    Fixing installation function call

--- js/projects.js
+++ js/projects.js
@@ -3,13 +3,16 @@
   var storeButtons = document.getElementsByClassName('webstore');
   for (var i = 0; i < storeButtons.length; i++) {
     storeButtons[i].onclick = function() { 
-      chrome.webstore.install(function() {
-        this.classList.add('added');
-        this.innerText = 'ADDED TO CHROME';
-      },
-      function() {
-        this.innerHTML = '<div></div>ADD TO CHROME';
-      });
+      chrome.webstore.install(
+        "https://chrome.google.com/webstore/detail/aafbnclakfldlfneckdnnaojkeepdpcg",
+        function() {
+          this.classList.add('added');
+          this.innerText = 'ADDED TO CHROME';
+        },
+        function() {
+          this.innerHTML = '<div></div>ADD TO CHROME';
+        }
+      );
       this.innerText = 'CHECKING...';
     }
   }
