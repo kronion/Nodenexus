1466b7b1d24b42fcd196dfd1ba21eb507f4ed584
Collin Stedman <cstedman@princeton>
2015-03-17 16:43:22 -0500

    Testing extension detection

--- js/projects.js
+++ js/projects.js
@@ -1,22 +1,46 @@
 (function() {
   iFrameResize({ 'heightCalculationMethod': 'lowestElement' });
+
+  // IF USING CHROME BROWSER
+  var detect = function(base, absent, present) {
+    var s = document.createElement('link');
+    document.head.appendChild(s);
+    s.onerror = absent;
+    s.onload = present;
+    s.rel = 'subresource';
+    s.href = 'chrome-extension://' + base + '/PRESENT';
+  }
+
   var storeButtons = document.getElementsByClassName('webstore');
   for (var i = 0; i < storeButtons.length; i++) {
-    storeButtons[i].onclick = function() { 
-      var self = this;
-      chrome.webstore.install(
-        "https://chrome.google.com/webstore/detail/aafbnclakfldlfneckdnnaojkeepdpcg",
-        function() {
-          self.classList.add('added');
-          self.tabIndex = "-1";
-          self.innerText = 'ADDED TO CHROME';
-        },
-        function() {
-          self.innerHTML = '<div></div>ADD TO CHROME';
-        }
-      );
-      self.blur();
-      self.innerText = 'CHECKING...';
-    }
+    var btn = storeButtons[i];
+    var storeId = btn.dataset.storeid; 
+    detect(storeId,
+      function(btn) {
+        btn.innerHTML = '<div></div>ADD TO CHROME';
+        btn.onclick = function() {
+          chrome.webstore.install(
+            "https://chrome.google.com/webstore/detail/" + btn.dataset.storeid,
+            function() {
+              btn.classList.add('added');
+              btn.tabIndex = "-1";
+              btn.onclick = null;
+              btn.innerText = 'ADDED TO CHROME';
+            },
+            function() {
+              btn.style.pointerEvents="auto";
+              btn.innerHTML = '<div></div>ADD TO CHROME';
+            }
+          );
+          btn.style.pointerEvents="none";
+          btn.innerText = 'CHECKING...';
+        };
+      }.bind(this, btn),
+      function(btn) {
+        btn.classList.add('added');
+        btn.tabIndex = "-1";
+        btn.innerText = 'ADDED TO CHROME';
+      }.bind(this, btn)
+    );
   }
 })();
