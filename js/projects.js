65fcd5593088bfac60ff330d10f54da774bc11bc
Collin Stedman <cstedman@princeton>
2015-03-16 19:56:03 -0500

    Problem with 'this'

--- js/projects.js
+++ js/projects.js
@@ -3,17 +3,18 @@
   var storeButtons = document.getElementsByClassName('webstore');
   for (var i = 0; i < storeButtons.length; i++) {
     storeButtons[i].onclick = function() { 
+      var self = this;
       chrome.webstore.install(
         "https://chrome.google.com/webstore/detail/aafbnclakfldlfneckdnnaojkeepdpcg",
         function() {
-          this.classList.add('added');
-          this.innerText = 'ADDED TO CHROME';
+          self.classList.add('added');
+          self.innerText = 'ADDED TO CHROME';
         },
         function() {
-          this.innerHTML = '<div></div>ADD TO CHROME';
+          self.innerHTML = '<div></div>ADD TO CHROME';
         }
       );
-      this.innerText = 'CHECKING...';
+      self.innerText = 'CHECKING...';
     }
   }
 })();
