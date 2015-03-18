a726d265ce982f60678bfb199947d8005e8bfd81
Collin Stedman <cstedman@princeton>
2015-03-18 01:54:55 -0500

    Extensions are Chrome-only

--- js/projects.js
+++ js/projects.js
@@ -1,48 +1,55 @@
 (function() {
   iFrameResize({ 'heightCalculationMethod': 'lowestElement' });
 
-  // IF USING CHROME BROWSER
-  var detect = function(base, absent, present) {
-    var s = document.createElement('link');
-    document.head.appendChild(s);
-    s.onerror = absent;
-    s.onload = present;
-    s.rel = 'subresource';
-    s.href = 'chrome-extension://' + base + '/PRESENT';
-  }
-
   var storeButtons = document.getElementsByClassName('webstore');
-  for (var i = 0; i < storeButtons.length; i++) {
-    var btn = storeButtons[i];
-    var storeId = btn.dataset.storeid; 
-    detect(storeId,
-      function(btn) {
-        btn.innerHTML = '<div></div>ADD TO CHROME';
-        btn.onclick = function() {
-          btn.blur();
-          btn.classList.add('checking');
-          btn.innerText = 'CHECKING...';
-          chrome.webstore.install(
-            "https://chrome.google.com/webstore/detail/" + btn.dataset.storeid,
-            function() {
-              btn.classList.remove('checking');
-              btn.classList.add('added');
-              btn.tabIndex = "-1";
-              btn.onclick = null;
-              btn.innerText = 'ADDED TO CHROME';
-            },
-            function() {
-              btn.classList.remove('checking');
-              btn.innerHTML = '<div></div>ADD TO CHROME';
-            }
-          );
-        };
-      }.bind(this, btn),
-      function(btn) {
-        btn.classList.add('added');
-        btn.tabIndex = "-1";
-        btn.innerText = 'ADDED TO CHROME';
-      }.bind(this, btn)
-    );
+  if (window.chrome && typeof window.orientation === 'undefined') {
+    var detect = function(base, absent, present) {
+      var s = document.createElement('link');
+      document.head.appendChild(s);
+      s.onerror = absent;
+      s.onload = present;
+      s.rel = 'subresource';
+      s.href = 'chrome-extension://' + base + '/PRESENT';
+    };
+    for (var i = 0; i < storeButtons.length; i++) {
+      var btn = storeButtons[i];
+      var storeId = btn.dataset.storeid; 
+      detect(storeId,
+        function(btn) {
+          btn.innerHTML = '<div></div>ADD TO CHROME';
+          btn.onclick = function() {
+            btn.blur();
+            btn.classList.add('checking');
+            btn.textContent = btn.innerText = 'CHECKING...';
+            chrome.webstore.install(
+              "https://chrome.google.com/webstore/detail/" + btn.dataset.storeid,
+              function() {
+                btn.classList.remove('checking');
+                btn.classList.add('added');
+                btn.tabIndex = "-1";
+                btn.onclick = null;
+                btn.textContent = btn.innerText = 'ADDED TO CHROME';
+              },
+              function() {
+                btn.classList.remove('checking');
+                btn.innerHTML = '<div></div>ADD TO CHROME';
+              }
+            );
+          };
+        }.bind(this, btn),
+        function(btn) {
+          btn.classList.add('added');
+          btn.tabIndex = "-1";
+          btn.textContent = btn.innerText = 'ADDED TO CHROME';
+        }.bind(this, btn)
+      );
+    }
+  }
+  else {
+    for (var i = 0; i < storeButtons.length; i++) {
+      var btn = storeButtons[i];
+      btn.textContent = btn.innerText = 'AVAILABLE ON CHROME';
+      btn.onclick = function() { window.open('https://www.google.com/chrome/', '_blank'); };
+    }
   }
 })();
